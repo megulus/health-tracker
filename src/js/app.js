861af23f1fa70b71c6abb12801cbebcbba928cd5
Meg Dahlgren <meg.dahlgren@gmail.com>
2016-04-11 13:58:16 -0400

    Write README, add build tools for performance optimization.

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,6 @@
 var app = app || {};
 var today = new Date();
 var todayMinusSeven = new Date(new Date().setDate(new Date().getDate() - 7));
-//var todayDateMinusSeven = todayMinusSeven.getDate();
-//var todayWeekday = today.getDay();
 var todayDate = today.getDate();
 var todayMonth = today.getMonth();
 var todayYear = today.getFullYear();
