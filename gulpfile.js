c07974d1552c71427373057360962065d7edcc54
Meg Dahlgren <meg.dahlgren@gmail.com>
2016-04-20 15:11:24 -0400

    Add gulp deploy task to deploy to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
     mainBowerFiles = require('main-bower-files'),
     usemin = require('gulp-usemin'),
     rename = require('gulp-rename'),
-    debug = require('gulp-debug');
+    debug = require('gulp-debug'),
+    ghpages = require('gulp-gh-pages');
 
 
 function errorLog(error) {
@@ -14,7 +15,7 @@ function errorLog(error) {
 }
 
 
-gulp.task('bower', function () {
+gulp.task('bower', function() {
     return gulp.src(mainBowerFiles(), {base: 'bower_components'})
         .pipe(debug({ title: 'bower-debug' }))
         .pipe(gulp.dest('build/bower_components'))
@@ -30,6 +31,12 @@ gulp.task('usemin', function() {
              js: [uglify(), 'concat']
          }))
          .pipe(gulp.dest('build'))
+});
+
+gulp.task('deploy', function() {
+    return gulp.src('./build/**/*')
+        .pipe(debug({ title: 'deploy-debug' }))
+        .pipe(ghpages())
 });
 
 
